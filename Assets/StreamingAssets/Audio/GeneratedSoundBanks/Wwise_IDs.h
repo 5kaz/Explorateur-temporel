72ecc29aa9d009ee6fa6b38024a38ad275b9da7c
FerdinandChupin <ferdinand.gonzague.chupin@gmail.com>
2022-10-08 19:38:21 +0200

    sound

---
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,48 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID AMB = 1117531639U;
+        static const AkUniqueID FOOTSTEP_STONE = 310261441U;
+        static const AkUniqueID FOOTSTEP_WATER = 4154792307U;
+        static const AkUniqueID FOOTSTEP_WOOD = 606549629U;
+        static const AkUniqueID UI_CLOCK_TOPAST = 1702484864U;
+        static const AkUniqueID UI_CLOCK_TOPRES = 1666516870U;
+    } // namespace EVENTS
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID PAST_PRES = 3309483646U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID MAIN = 3161908922U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
