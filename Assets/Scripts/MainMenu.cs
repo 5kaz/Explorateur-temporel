0a9655f7740c9f9dc1139f78ec8869019c05b1ac
AntoineA67 <adamy.antoine@gmail.com>
2022-10-09 15:07:21 +0200

    box colliders

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -20,7 +20,7 @@ public class MainMenu : MonoBehaviour {
     }
     public void startGame()
     {
-        SceneManager.LoadScene(0);
+        SceneManager.LoadScene("SampleSceneClem 1");
     }
 
     public void exitGame()
